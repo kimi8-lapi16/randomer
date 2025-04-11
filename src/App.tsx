496b9667abd19d342001e04aec40db581f910299
kimi8-lapi16 <takayoshi.kimishima@gmail.com>
2025-04-11 00:35:20 +0000

    chore: style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
 import RandomSelector from './Selector';
+import { GlobalStyle } from './commonStyle';
 
 export default function App() {
   return (
-    <main>
-      <h1>Random Selector</h1>
-      <RandomSelector />
-    </main>
+    <>
+      <GlobalStyle />
+      <Header>
+        <Title>Random Selector</Title>
+      </Header>
+      <MainContainer>        
+        <RandomSelector />
+      </MainContainer>
+      <Footer>
+        @kimi8-lapi16
+      </Footer>
+    </>
   );
 }
+
+const MainContainer = styled.main`
+  background-color: #EEEEEE;
+  width: 100%;
+  height: 100%;
+  margin-block: 1rem;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5rem;
+  background-color: var(--accent-color);
+  color: white;
+  margin-block: 0px;
+  padding: 1rem;
+  
+`;
+
+const Header = styled.header`
+`;
+
+const Footer = styled.footer`
+  text-align: center;
+`;
+
--- src/Selector.tsx
+++ src/Selector.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import Lottery from './Lottery';
 import Member from './Member';
 
-const options = ['片手剣', '双剣', '太刀', '大剣'];
+const options = ['片手剣', '双剣', '太刀', '大剣', 'ハンマー', '狩猟笛', 'ランス', 'ガンランス', 'スラッシュアックス', 'チャージアックス', '操虫棍', 'ライトボウガン', 'ヘビィボウガン', '弓'];
+
 export default function RandomSelector() {
   const [members, setMembers] = useState<string[]>([]);
   const callback = (value: string) => {
