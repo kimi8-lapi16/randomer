9761f919b237292f04d0d8514f3829648a5818ec
kimi8-lapi16 <takayoshi.kimishima@gmail.com>
2025-04-11 00:34:22 +0000

    feat: create button component

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,1 +1,22 @@
-export default function Button() {}
+import React, { ButtonHTMLAttributes } from "react";
+import styled from "styled-components";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & ButtonOptionalProps;
+
+type ButtonOptionalProps = {
+  label: string;
+};
+
+export default function Button(props: ButtonProps) {
+  const { label, ...remain } = props
+  return <StyledButton {...remain}>{label}</StyledButton>
+}
+
+const StyledButton = styled.button`
+  border-radius: 8px;
+  border-color: transparent;
+  color: var(--secondary-color);
+  background-color: var(--primary-color);
+  cursor: pointer;
+  padding-inline: 1rem;
+`;
---
+++ src/commonStyle.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --accent-color: #344699;
+    --primary-color: #FBFCFF;
+    --secondary-color: #2B262E;
+  }
+`;
